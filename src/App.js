989aee4de6c2c25f762f19d3e6124543b986c25f
aldopqp <aldopqp@gmail.com>
2025-03-09 10:54:21 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RentalCalculator from './components/RentalCalculator';
+
+function App() {
+  return (
+    <div>
+      <RentalCalculator />
+    </div>
+  );
+}
+
+export default App;
